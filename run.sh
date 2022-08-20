0e8ade301022bad2a62df86cb9d6110b0956f1b3
Abhay <2002abhaysharma@gmail.com>
2022-08-20 14:06:32 +0530

    first commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#!/run
+DIRECTORY=$(cd `dirname $0` && pwd)
+cd $DIRECTORY/sentinelfrontend
+source $DIRECTORY/venv/bin/activate
+$DIRECTORY/venv/bin/python3 $DIRECTORY/run.py & 
+
+sleep 5
+$DIRECTORY/venv/bin/python3 $DIRECTORY/sentinelfrontend/main.py
+
